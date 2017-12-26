b215c4080db4ac8c086acecbd0764d4abc7cdce1
shusong <shusong@qq.com>
2017-12-26 15:01:03 +0800

    first commit express -e

---
+++ conf/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dbhost: 'localhost',
+    dbport: '3306',
+    dbname: 'blogdata',
+    dbuser: 'root',
+    dbpassword: 'ss123',
+    sqlversion: '2012',
+    secret: 'severApi',
+    debug: true,
+    port: 3306
+};
---
+++ utils/dbmysql.js
@@ -0,0 +1,39 @@
+const mysql=require('mysql')
+const cfg=require('../conf')
+const pool = mysql.createPool({
+    connectionLimit : 10,  
+    host: cfg.dbhost,  
+    user: cfg.dbuser, 
+    password: cfg.dbpassword,  
+    database: cfg.dbname,
+    multipleStatements: true,  
+    port: cfg.dbport || 3306  
+}); 
+
+
+pool.getConnection(function (err,connection) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+
+function query(){
+    return new Promise((resolve, reject) => {
+        var args = Array.prototype.slice.call(arguments);
+        args.push(function (error, results, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+        pool.query.apply(pool, args);
+    });
+}
+
+module.exports = {
+    query:query
+};
